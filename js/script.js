efedcb3d68f87b041d8643026d16a4adddb0168c
Tanjil Rahman <57291935+tanjilrahman321@users.noreply.github.com>
2021-09-30 12:30:19 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    // Counter Up
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+ // Aos Js
+      AOS.init({
+        offset: 120,
+        duration: 1000,
+      });
+
+});
